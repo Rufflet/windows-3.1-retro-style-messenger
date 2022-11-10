d3ab6939d8058daab0384c37ad64ba44152f5cec
Alexander Ershov <ershov_as@sbisoft.ru>
2022-11-10 23:29:06 +0300

    minor fixes (heroku deploy)

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const { PORT = 3000 } = process.env;
 
 app.use(express.static("dist"));
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
 
 app.get("*", (request, response) => {
